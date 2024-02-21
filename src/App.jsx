c7618dbc27f6631b2c3405f8b9af6c927cd2505b
Connor03M <conboy0303@gmail.com>
2024-02-21 15:27:56 +0000

    uu

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 
 import {React} from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ReactDOM from 'react-dom';
 import Login from "./pages/shared/Login";
 import Register from "./pages/shared/Register";
 import Home from "./pages/shared/Home";
@@ -13,7 +14,7 @@ function App() {
     <Router>
       <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top" aria-label="Fourth navbar example">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Little Stars Medical Centre</a>
+    <a className="navbar-brand" href="#Home">Little Stars Medical Centre</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -21,13 +22,13 @@ function App() {
     <div className="collapse navbar-collapse" id="navbarsExample04">
       <ul className="navbar-nav me-auto mb-2 mb-md-0">
         <li className="nav-item">
-          <a className="nav-link active" href="#Home">Home</a>
+          <Link class="mx-2" to="/">Home</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#Login">Login</a>
+        <Link class="mx-2" to="/login">Login</Link>        
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#Register">Register</a>
+        <Link class="mx-2" to="/register">Register</Link>        
         </li>
       </ul>
     </div>
