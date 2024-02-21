7fdf755d7ae864cb38c986c2e84f5459b879488d
Connor03M <conboy0303@gmail.com>
2024-02-21 14:31:24 +0000

    hh

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,55 @@
 // src/App.js
 
-import React from "react";
+import {React} from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from "./pages/shared/Login";
 import Register from "./pages/shared/Register";
 import Home from "./pages/shared/Home";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import './App.css';
 
-function ColorSchemesExample() {
+function App() {
   return (
-    <>
-      <Navbar fixed="top" bg="light" data-bs-theme="light">
-        <Container>
-          <Navbar.Brand href="#home">Hospital</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/pages/shared/Home.jsx">Home</Nav.Link>
-            <Nav.Link href="/pages/shared/Login.jsx">Login</Nav.Link>
-            <Nav.Link href="/pages/shared/Register.jsx">Register</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </>
+    <Router>
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark" aria-label="Fourth navbar example">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Expand at md</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+
+    <div className="collapse navbar-collapse" id="navbarsExample04">
+      <ul className="navbar-nav me-auto mb-2 mb-md-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+        </Routes>
+      </div>
+      </Router>
+    
   );
 }
-
-export default ColorSchemesExample;
+export default App;
