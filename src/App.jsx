717b36c4e7be25014758c635f7a98ddcc9c502ba
Connor03M <conboy0303@gmail.com>
2024-02-21 11:58:48 +0000

    mm

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,25 @@ import Login from "./pages/shared/Login";
 import Register from "./pages/shared/Register";
 import Home from "./pages/shared/Home";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-const App = () => {
+function ColorSchemesExample() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <hr />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </div>
-    </Router>
+    <>
+      <Navbar fixed="top" bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   );
-};
-export default App;
+}
+
+export default ColorSchemesExample;
