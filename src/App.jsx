b6ef7ce4f4499180ea18f56cfb4644d7a37adea8
Connor03M <conboy0303@gmail.com>
2024-02-21 15:10:05 +0000

    landing

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,9 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <nav className="navbar navbar-expand-md navbar-dark bg-dark" aria-label="Fourth navbar example">
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top" aria-label="Fourth navbar example">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Expand at md</a>
+    <a className="navbar-brand" href="#">Little Stars Medical Centre</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -21,21 +21,13 @@ function App() {
     <div className="collapse navbar-collapse" id="navbarsExample04">
       <ul className="navbar-nav me-auto mb-2 mb-md-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active" href="#Home">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link" href="#Login">Login</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+          <a className="nav-link" href="#Register">Register</a>
         </li>
       </ul>
     </div>
--- src/pages/shared/Home.jsx
+++ src/pages/shared/Home.jsx
@@ -2,7 +2,14 @@ import React from "react";
 
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <><h1>Little Stars Medical Centre</h1>
+        <p1>At Little Stars Medical Centre, we provide
+             specialized pediatric care in a nurturing 
+             environment. Our experienced team of pediatric 
+             specialists offers expert medical care tailored 
+             to the unique needs of children, from routine 
+             check-ups to complex treatments.</p1>
+        </>
     )
 }
 export default Home
